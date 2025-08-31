caa93181631de2e97624c2dcaca781e6473ab662
aokumo <139054327+aokumo21@users.noreply.github.com>
2025-08-31 20:21:50 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,21 @@
+preGameLauncher.setConfig(
+    preGameLauncher.createConfig(
+        "testString",
+        "hewo. Im a test string :3",
+        0,
+        10
+    ),
+    preGameLauncher.createConfig(
+        "testNumber",
+        6,
+        5,
+        15
+    ),
+    preGameLauncher.createConfig(
+        "testBoolean",
+        true,
+        0,
+        0
+    )
+)
 preGameLauncher.run(null, null, true)
